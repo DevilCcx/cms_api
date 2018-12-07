23d573f594335e66d32021834d9d57baeb66cc30
GuoFang1994 <13659215849@163.com>
2018-12-08 00:06:08 +0800

    增加auth数据库json

--- koa.config.js
+++ koa.config.js
@@ -1,5 +1,6 @@
 const koa = {
-    article_dir: '/Users/devil/docker/wwwroot/blog/source/_posts',
+    // article_dir: '/Users/devil/docker/wwwroot/blog/source/_posts',
+    article_dir: '/www/devil_blog/source/_posts',
     article_draft_dir: '/Users/devil/docker/wwwroot/blog/source/_drafts'
 };
 
